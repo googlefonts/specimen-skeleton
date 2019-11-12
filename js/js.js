4d7c11b0bb64d90ca5acc0e7812f867e3255fece
Roel Nieskens <roel@pixelambacht.nl>
2019-11-12 16:55:02 +0100

    Add basic fontfaceobserver setup

--- js/js.js
+++ js/js.js
@@ -1,1 +1,13 @@
-console.log("JavaScript!");
+const fontName = "Fraunces";
+const fontTimeOut = 5000; // In milliseconds
+
+const font = new FontFaceObserver(fontName);
+font.load(null, fontTimeOut).then(
+	() => {
+		console.log("Font is available");
+		document.documentElement.className += " fonts-loaded";
+	},
+	() => {
+		console.log("Font is not available");
+	}
+);
