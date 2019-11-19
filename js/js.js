5d9860d6463a453616f99ac14e9032f328db65c0
Roel Nieskens <roel@pixelambacht.nl>
2019-11-19 11:51:28 +0100

    Add basic interactive text w/ variable sliders
    Plus move font-specific data to _data folder

--- js/js.js
+++ js/js.js
@@ -1,6 +1,7 @@
 const fontName = "Fraunces";
 const fontTimeOut = 5000; // In milliseconds
 
+// Set up FontFaceObserver
 const font = new FontFaceObserver(fontName);
 font.load(null, fontTimeOut).then(
 	() => {
@@ -11,3 +12,19 @@ font.load(null, fontTimeOut).then(
 		console.log("Font is not available");
 	}
 );
+
+// Interactive contols (sliders that tweak axes)
+const interactives = document.querySelectorAll(".interactive-controls");
+for (const interactive of interactives) {
+	const area = interactive.querySelector(".interactive-controls-text");
+
+	const varset = el => {
+		area.style.setProperty("--" + el.name, el.value);
+	};
+
+	const sliders = interactive.querySelectorAll("input[type=range]");
+	for (const slider of sliders) {
+		varset(slider);
+		slider.oninput = e => varset(e.target);
+	}
+}
