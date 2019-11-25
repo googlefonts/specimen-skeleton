a57aa453322e5ca27715eb28ee90999c5124ce3a
Roel Nieskens <roel@pixelambacht.nl>
2019-11-25 16:17:19 +0100

    Add grid mouseover effect
    Introduced throttling function

--- js/js.js
+++ js/js.js
@@ -1,6 +1,25 @@
 const fontName = "Fraunces";
 const fontTimeOut = 5000; // In milliseconds
 
+// Generic functions
+// From https://gist.github.com/beaucharman/e46b8e4d03ef30480d7f4db5a78498ca#gistcomment-3015837
+const throttle = (fn, wait) => {
+	let previouslyRun, queuedToRun;
+
+	return function invokeFn(...args) {
+		const now = Date.now();
+
+		queuedToRun = clearTimeout(queuedToRun);
+
+		if (!previouslyRun || now - previouslyRun >= wait) {
+			fn.apply(null, args);
+			previouslyRun = now;
+		} else {
+			queuedToRun = setTimeout(invokeFn.bind(null, ...args), wait - (now - previouslyRun));
+		}
+	};
+};
+
 // Set up FontFaceObserver
 const font = new FontFaceObserver(fontName);
 font.load(null, fontTimeOut).then(
@@ -41,3 +60,12 @@ if ("IntersectionObserver" in window) {
 		obs.observe(el);
 	});
 }
+
+// Character grid
+const grid = document.querySelector(".character-grid");
+const gridzoom = document.querySelector(".character-grid-zoom");
+grid.onmousemove = throttle(e => {
+	if (e.target.tagName === "LI") {
+		gridzoom.innerHTML = e.target.innerHTML;
+	}
+}, 100);
