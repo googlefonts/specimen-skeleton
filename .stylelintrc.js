8666ab7cd1452e98ebef1b775ac87fac7571f866
Roel Nieskens <roel@pixelambacht.nl>
2020-01-27 14:06:36 +0100

    Enforce kebab-case selectors
    As per:
    https://github.com/stylelint/stylelint/issues/1951

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -2,6 +2,7 @@ module.exports = {
 	extends: ["stylelint-config-standard", "stylelint-config-prettier"],
 	plugins: ["stylelint-no-unsupported-browser-features"],
 	rules: {
+		"selector-class-pattern": "^([a-z][a-z0-9]*)(-[a-z0-9]+)*$",
 		indentation: "tab",
 		"selector-pseudo-class-no-unknown": [
 			true,
